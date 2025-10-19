47c325cc7f7aeaae9cf8e9e05eba2b805ba22c82
Carlos Marcelo Nieva <marcelonieva7@gmail.com>
2025-10-19 22:37:08 +0000

    ref: use eval winners parametres

--- src/RAG/main.py
+++ src/RAG/main.py
@@ -2,8 +2,8 @@ from src.config.db import qdrant_db
 from src.LLM.main import chat
 from src.RAG.prompts import build_user_prompt, system_prompt_v1
 
-def rag(query, model='openai/gpt-oss-20b'):
-    retrival = qdrant_db.search(query, limit=10, type='hybrid', fusion='DBSF')
+def rag(query, model='meta/llama-4-scout-17b-16e-instruct'):
+    retrival = qdrant_db.search(query, limit=10, type='hybrid', fusion='RRF')
     user = build_user_prompt(query, retrival)
     answer = chat(system=system_prompt_v1, user=user, model=model)
 
