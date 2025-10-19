333525961b7406fba3f3c35eddc41adf878a7507
Carlos Marcelo Nieva <marcelonieva7@gmail.com>
2025-10-19 08:12:02 +0000

    ref: add system prompt to rag flow

--- src/RAG/main.py
+++ src/RAG/main.py
@@ -1,10 +1,10 @@
 from src.config.db import qdrant_db
 from src.LLM.main import chat
-from src.RAG.prompts import build_user_prompt
+from src.RAG.prompts import build_user_prompt, system_prompt_v1
 
-def rag(query, system_prompt, model='openai/gpt-oss-20b'):
+def rag(query, model='openai/gpt-oss-20b'):
     retrival = qdrant_db.search(query, limit=10, type='hybrid', fusion='DBSF')
     user = build_user_prompt(query, retrival)
-    answer = chat(system=system_prompt, user=user, model=model)
+    answer = chat(system=system_prompt_v1, user=user, model=model)
 
     return answer
