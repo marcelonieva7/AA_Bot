a31602aec920c61f23d23950c1006bf9421edaa8
Carlos Marcelo Nieva <marcelonieva7@gmail.com>
2025-10-19 22:37:23 +0000

    feat: add main script

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
-if __name__ == "__main__":
+from src.server.app import app
+
+
+
+""" if __name__ == "__main__":
   import uvicorn
   uvicorn.run('src.server.app:app', host="0.0.0.0", port=8000)
+ """
